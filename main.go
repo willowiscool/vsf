8c831f120952bf6f34449760058f699907a0f133
Victor Veytsman <vityavv@gmail.com>
2019-02-20 19:27:13 -0500

    Font & can use different files

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"github.com/faiface/pixel/imdraw"
+	"github.com/faiface/pixel/text"
+	"golang.org/x/image/font/basicfont"
+	"github.com/yuin/gopher-lua"
 
 	"math/rand"
 	"image/color"
-
-	"github.com/yuin/gopher-lua"
 	"time"
 	"fmt"
+	"os"
 )
 const (
 	LIST_LENGTH = 500 //the length of the list to be sorted
@@ -18,12 +20,13 @@ const (
 	BLOCK_HEIGHT_MULT = 1 //the amount that the height of the block is multiplied by its position
 	WIDTH = LIST_LENGTH * BLOCK_WIDTH //the width of the window
 	HEIGHT = LIST_LENGTH * BLOCK_HEIGHT_MULT //the height of the window
-	SLEEP = 0 //how many milliseconds to sleep between showings
+	SLEEP = 1 //how many milliseconds to sleep between showings
 )
 var (
 	imd *imdraw.IMDraw
 	list = rand.Perm(LIST_LENGTH)
 	stop = make(chan byte, 1)
+	FILENAME = os.Args[1]
 )
 
 func run() {
@@ -45,9 +48,19 @@ func run() {
 	imd.Push(pixel.V(WIDTH, HEIGHT))
 	imd.Rectangle(0)
 
+	//filename
+	atlas := text.NewAtlas(
+		basicfont.Face7x13,
+		[]rune(FILENAME),
+		[]rune("VSF: "),
+		text.ASCII)
+	txt := text.New(pixel.V(0, HEIGHT - 26), atlas) //26 = 2 * height
+	fmt.Fprintf(txt, "VSF: %s", FILENAME)
+
 	for !win.Closed() {
-		win.Clear(color.RGBA{0x55, 0xaa, 0xaa, 0xff})
-		imd.Draw(win)
+		win.Clear(color.RGBA{0, 0, 0, 0xff})
+		//imd.Draw(win)
+		txt.Draw(win, pixel.IM.Scaled(txt.Orig, 2))
 		for i, val := range list {
 			rect := imdraw.New(nil)
 			rect.Color = pixel.RGB(1, 1, 1)
@@ -73,7 +86,6 @@ func show(L *lua.LState) int {
 	newList := make([]int, LIST_LENGTH)
 	exists := make(map[int]bool, LIST_LENGTH)
 	newTable.ForEach(func(a, b lua.LValue) {
-		fmt.Println(a, b)
 		if b.(lua.LNumber) <= 0 || b.(lua.LNumber) > LIST_LENGTH {
 			panic("Invalid value found in list")
 		}
@@ -98,7 +110,7 @@ func main() {
 		L := lua.NewState()
 		defer L.Close()
 		L.SetGlobal("show", L.NewFunction(show))
-		if err := L.DoFile("sort.lua"); err != nil {
+		if err := L.DoFile(FILENAME); err != nil {
 			panic(err)
 		}
 	}()
